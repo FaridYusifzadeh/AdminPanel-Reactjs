f21b03415a5d46075f315ee7a97a24836e32cb26
FaridYusifzadeh <fariddsx2015@gmail.com>
2021-11-16 16:35:09 +0400

    add hover icons and font size

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -5,23 +5,46 @@ export default function SideBar() {
   return (
     <div className="sidebar">
       <div className="sidebar_wrapper">
+        {/* Side Bar Menu */}
         <div className="sidebar_menu">
           <h3 className="sidebar_title">Dashboard</h3>
 
           <ul className="sidebar_list">
             {/* < ==== Home  =====> */}
-            <li className="sidebar_list_item">
-              <LineStyle />
+            <li className="sidebar_list_item active">
+              <LineStyle className="sidebar_icon" />
               Home
             </li>
             {/* < ====  Analytics =====> */}
             <li className="sidebar_list_item">
-              <Timeline />
+              <Timeline className="sidebar_icon" />
               Analytics
             </li>
             {/* < ====  Sales =====> */}
             <li className="sidebar_list_item">
-              <TrendingUp />
+              <TrendingUp className="sidebar_icon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        {/* Side Bar Menu */}
+        <div className="sidebar_menu">
+          <h3 className="sidebar_title">Dashboard</h3>
+
+          <ul className="sidebar_list">
+            {/* < ==== Home  =====> */}
+            <li className="sidebar_list_item ">
+              <LineStyle className="sidebar_icon" />
+              Home
+            </li>
+            {/* < ====  Analytics =====> */}
+            <li className="sidebar_list_item">
+              <Timeline className="sidebar_icon" />
+              Analytics
+            </li>
+            {/* < ====  Sales =====> */}
+            <li className="sidebar_list_item">
+              <TrendingUp className="sidebar_icon" />
               Sales
             </li>
           </ul>
