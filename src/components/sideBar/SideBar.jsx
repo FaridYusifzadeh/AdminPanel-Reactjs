abd18c4ff7bf7147a8d84282bf7214b66e88d760
FaridYusifzadeh <fariddsx2015@gmail.com>
2021-11-16 17:09:53 +0400

    add icon in menu

--- src/components/sideBar/SideBar.jsx
+++ src/components/sideBar/SideBar.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./SideBar.css";
-import { LineStyle, Timeline, TrendingUp } from "@material-ui/icons";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  Group,
+  Apartment,
+  AttachMoney,
+  Assessment,
+} from "@material-ui/icons";
 export default function SideBar() {
   return (
     <div className="sidebar">
       <div className="sidebar_wrapper">
-        {/* Side Bar Menu */}
+        {/*<==================== Dashboard ============== > */}
         <div className="sidebar_menu">
           <h3 className="sidebar_title">Dashboard</h3>
 
@@ -27,9 +35,36 @@ export default function SideBar() {
             </li>
           </ul>
         </div>
-        {/* Side Bar Menu */}
+        {/*<==================== Quick Menu============== > */}
         <div className="sidebar_menu">
-          <h3 className="sidebar_title">Dashboard</h3>
+          <h3 className="sidebar_title">Quick Menu</h3>
+
+          <ul className="sidebar_list">
+            {/* < ==== Users  =====> */}
+            <li className="sidebar_list_item ">
+              <Group className="sidebar_icon" />
+              Users
+            </li>
+            {/* < ====  Products =====> */}
+            <li className="sidebar_list_item">
+              <Apartment className="sidebar_icon" />
+              Products
+            </li>
+            {/* < ====  Transactions =====> */}
+            <li className="sidebar_list_item">
+              <AttachMoney className="sidebar_icon" />
+              Transactions
+            </li>
+            {/* < ====  Reports =====> */}
+            <li className="sidebar_list_item">
+              <Assessment className="sidebar_icon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        {/*<==================== Side Bar Menu ============== > */}
+        <div className="sidebar_menu">
+          <h3 className="sidebar_title">Notifications</h3>
 
           <ul className="sidebar_list">
             {/* < ==== Home  =====> */}
@@ -49,6 +84,29 @@ export default function SideBar() {
             </li>
           </ul>
         </div>
+        {/*<==================== Side Bar Menu ============== > */}
+        <div className="sidebar_menu">
+          <h3 className="sidebar_title">Staff</h3>
+
+          <ul className="sidebar_list">
+            {/* < ==== Home  =====> */}
+            <li className="sidebar_list_item ">
+              <LineStyle className="sidebar_icon" />
+              Home
+            </li>
+            {/* < ====  Analytics =====> */}
+            <li className="sidebar_list_item">
+              <Timeline className="sidebar_icon" />
+              Analytics
+            </li>
+            {/* < ====  Sales =====> */}
+            <li className="sidebar_list_item">
+              <TrendingUp className="sidebar_icon" />
+              Sales
+            </li>
+          </ul>
+        </div>
+        {/* < ================================================ >  */}
       </div>
     </div>
   );
