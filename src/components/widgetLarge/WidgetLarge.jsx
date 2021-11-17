cc64c7aa058663aeb139a0ccc4d3357621785311
FaridYusifzadeh <fariddsx2015@gmail.com>
2021-11-17 16:55:10 +0400

    add button component in widget large component

--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -1,4 +1,34 @@
 import "./WidgetLarge.css";
 export default function WidgetLarge() {
-  return <div className="widget_large">large</div>;
+  const Button = ({ type }) => {
+    return <button className={"widget_large_button" + type}>{type}</button>;
+  };
+  return (
+    <div className="widget_large">
+      <h3 className="widget_large_title">Latest Transactions</h3>
+      <table className="widget_large_table">
+        <tr className="widget_large_tr">
+          <th className="widget_large_th">Customer</th>
+          <th className="widget_large_th">Date</th>
+          <th className="widget_large_th">Amount</th>
+          <th className="widget_large_th">Status</th>
+        </tr>
+        <tr className="widget_large_tr">
+          <td className="widget_large_user">
+            <img
+              src="https://cdn-icons-png.flaticon.com/512/147/147144.png"
+              alt="avatar"
+              className="widget_large_img"
+            />
+            <span className="widget_large_name">Farid Yusifzadeh</span>
+          </td>
+          <td className="widget_large_date">2 June 2021</td>
+          <td className="widget_large_amount">$122.33</td>
+          <td className="widget_large_status">
+            <Button type="Approved" />
+          </td>
+        </tr>
+      </table>
+    </div>
+  );
 }
